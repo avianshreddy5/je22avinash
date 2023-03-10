de94ddeeee1a7596d091269d9475116edc27be59
admin <admin@DESKTOP-SFBVSET>
2023-03-10 23:10:03 +0530

    ayush

---
+++ ayush/src/main/java/ayush/loginservlet.java
@@ -0,0 +1,12 @@
+package ayush;
+
+import javax.servlet.http.*;
+
+public class loginservlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
